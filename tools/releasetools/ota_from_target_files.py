b0f531e2cc1060a08b080f1db7a8eb9ae0426008
rmcc <https://github.com/rmcc>
2015-12-10 14:49:04 +0200

    ota: Remove cruft we don't care about
    Skip the date check (downgrade to your content), and don't include
    recovery in otapackages.
    5.0.0 update: We want the recovery patchstuff back
    
    Change-Id: I51b48f658d1d25969189f700a176a0b9504273dd

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -534,11 +534,6 @@ def WriteFullOTAPackage(input_zip, output_zip):
   has_recovery_patch = HasRecoveryPatch(input_zip)
   block_based = OPTIONS.block_based
 
-  if not OPTIONS.omit_prereq:
-    ts = GetBuildProp("ro.build.date.utc", OPTIONS.info_dict)
-    ts_text = GetBuildProp("ro.build.date", OPTIONS.info_dict)
-    script.AssertOlderBuild(ts, ts_text)
-
   AppendAssertions(script, OPTIONS.info_dict, oem_dict)
   device_specific.FullOTA_Assertions()
 
